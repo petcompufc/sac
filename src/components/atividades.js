fecc0240cdb350600a1bc97e002504fe20389162
Mayron Alves <61197874+Mayron5@users.noreply.github.com>
2020-08-20 12:26:17 -0300

    :art: Timeline responsiva

--- src/components/atividades.js
+++ src/components/atividades.js
@@ -3,6 +3,7 @@ const atividades = [
         dia: 'Terça-feira (08/09)',
         dados: [
             {
+                tipo: 'Palestra',
                 titulo: 'Palestra 01',
                 ministrante: '',
                 desc: '',
@@ -10,10 +11,11 @@ const atividades = [
                 hora: '16:00 - 16:50'
             },
             {
+                tipo: 'Mini-Curso',
                 titulo: 'Palestra 02',
                 ministrante: 'Andreza',
-                desc: '',
-                plataforma: '',
+                desc: 'Ela ainda vai dizer, eu acho skaksa',
+                plataforma: 'Google Meet',
                 hora: '17:00 - 17:50'
             },
         ]
@@ -22,6 +24,7 @@ const atividades = [
         dia: 'Quarta-feira (09/09)',
         dados: [
             {
+                tipo: 'Mesa Redonda',
                 titulo: 'Palestra 03',
                 ministrante: '',
                 desc: '',
@@ -29,6 +32,7 @@ const atividades = [
                 hora: '16:00 - 16:50'
             },
             {
+                tipo: 'Workshop',
                 titulo: 'Palestra 04',
                 ministrante: '',
                 desc: '',
@@ -41,6 +45,7 @@ const atividades = [
         dia: 'Quinta-feira (10/09)',
         dados: [
             {
+                tipo: 'Palestra',
                 titulo: 'Palestra 05',
                 ministrante: '',
                 desc: '',
@@ -48,6 +53,7 @@ const atividades = [
                 hora: '16:00 - 16:50'
             },
             {
+                tipo: 'Palestra',
                 titulo: 'Palestra 06',
                 ministrante: 'Vida de programador',
                 desc: '',
@@ -60,6 +66,7 @@ const atividades = [
         dia: 'Sexta-feira (11/09)',
         dados: [
             {
+                tipo: 'Palestra',
                 titulo: 'Palestra 08',
                 ministrante: '',
                 desc: '',
@@ -67,6 +74,7 @@ const atividades = [
                 hora: '16:00 - 16:50'
             },
             {
+                tipo: 'Palestra',
                 titulo: 'Palestra 09',
                 ministrante: 'Italiano',
                 desc: '',
@@ -74,6 +82,7 @@ const atividades = [
                 hora: '17:00 - 17:50'
             },
             {
+                tipo: 'VGO',
                 titulo: 'Gartic + Webgames',
                 ministrante: '',
                 desc: '',
@@ -86,6 +95,7 @@ const atividades = [
         dia: 'Sábado (12/09)',
         dados: [
             {
+                tipo: 'VGO',
                 titulo: 'Campeonato de LOL',
                 ministrante: '',
                 desc: '',
@@ -93,6 +103,7 @@ const atividades = [
                 hora: '16:00 - 17:50'
             },
             {
+                tipo: 'VGO',
                 titulo: 'Compeonato de Valorant',
                 ministrante: '',
                 desc: '',
@@ -106,6 +117,7 @@ const atividades = [
         dia: 'Domingo (13/03)',
         dados: [
             {
+                tipo: 'VGO',
                 titulo: 'Campeonato de CS:GO',
                 ministrante: '',
                 desc: '',
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -12,77 +12,117 @@ import {
   AccordionHeader,
   AccordionPanel,
   AccordionIcon,
-  Box
+  Box,
+  Divider
 } from '@chakra-ui/core';
 
 
 function timeline({ atividades }) {
+  const isBiggerOf744 = styleMedia.matchMedium('(min-width: 744px)')
   return (
-
-    <Heading
-      size="lg"
-      marginTop={50}
-      marginLeft={100}
-      marginRight={100}
-    >
-      <Text
-        fontSize='auto'
-        textAlign='center'
-        borderBottom={{ style: 'solid', width: '2px' }}
-        borderBottomColor='#cc6600'
+    <Box flexGrow={1} flexShrink={1} >
+      <Heading
+        size="2x1"
+        maxW={250}
+        marginTop={50}
+        marginLeft={75}
+        marginRight={50}
       >
-        PROGRAMAÇÃO
-      </Text>
-      <Tabs
-        isFitted
-        variant="enclosed"
-        variantColor='#cc6600'
-        borderColor="#cc6600"
-        marginTop={10}
-      >
-        <TabList>
-          {atividades.map((dia, index) => (
-            <Tab color='#cc6600' _hover={{ bg: '#cc6600', color: 'white' }}> {dia.dia} </Tab>
-          ))}
-        </TabList>
-        <TabPanels
-          borderLeft={{ style: 'solid', width: '1px' }}
-          borderLeftColor='#cc6600'
-          borderBottom={{ style: 'solid', width: '1px' }}
-          borderBottomColor='#cc6600'
-          borderRight={{ style: 'solid', width: '1px' }}
-          borderRightColor='#cc6600'
-          padding={5}
+        <Text textAlign='center'>PROGRAMAÇÃO</Text>
+        <Divider
+          borderColor='#cc6600'
+          borderWidth={5}
+        />
+      </Heading >
+      <Box>
+        <Tabs
+          isFitted
+          variant={isBiggerOf744 ? 'enclosed' : 'unstyled'}
+          _selected={{ color: "white", bg: "blue.500" }}
+          variantColor='#cc6600'
+          borderColor="#cc6600"
+          margin={3}
+          orientation={isBiggerOf744 ? 'horizontal' : 'vertical'}
         >
-          {
-            atividades.map((atividade) => (
-              <TabPanel>
-                <Accordion allowMultiple>
-                  {atividade.dados.map((dados) => (
-                    <AccordionItem >
-                      <AccordionHeader bg='#cc6600' color='white' mt={2} _hover>
-                        <Box flex="1" textAlign="left">
-                          {dados.hora} | {dados.titulo}
-                        </Box>
-                        <AccordionIcon />
-                      </AccordionHeader>
-                      <AccordionPanel pb={4} >
-                        <center>Ministrante:</center> {dados.ministrante}
-                        <br />
-                        <center>Descrição:</center> {dados.desc}
-                        <br />
-                        <center>Plataforma: </center>{dados.plataforma}
-                      </AccordionPanel>
-                    </AccordionItem>
-                  ))}
+          <TabList>
+            {atividades.map((dia) => (
+              <Tab color='#cc6600'
+                _hover={{ bg: '#cc6600', color: 'white' }}
+                _selected={isBiggerOf744 ? ({ bg: 'white', color: '#CC6600', border:'1px', borderBottom:'none' }) : ({ bg: '#CC6600', color: 'white' })}
+              >
+                {dia.dia}
+              </Tab>
+            ))}
+          </TabList>
+          <TabPanels
+            borderTop={isBiggerOf744 ? '' : ({style: 'solid', width: '1px'})}
+            borderTopColor='#cc6600'
+            borderLeft={{ style: 'solid', width: '1px' }}
+            borderLeftColor='#cc6600'
+            borderBottom={{ style: 'solid', width: '1px' }}
+            borderBottomColor='#cc6600'
+            borderRight={{ style: 'solid', width: '1px' }}
+            borderRightColor='#cc6600'
+            padding={5}
+          >
+            {
+              atividades.map((atividade) => (
+                <TabPanel>
+                  <Accordion allowMultiple>
+                    {atividade.dados.map((dados) => (
+                      <AccordionItem >
+                        <AccordionHeader bg='#cc6600' color='white' mt={2} _hover>
+                          <Box flex="1" textAlign="left" >
+                            {dados.hora} | {dados.tipo} | {dados.titulo}
+                          </Box>
+                          <AccordionIcon />
+                        </AccordionHeader>
+                        <AccordionPanel pb={4} >
+                          <Text
+                            fontSize='2x1'
+                            color='#cc6600'
+                            display={dados.tipo == 'VGO' ? 'none' : ''}
+                          >
+                            {dados.tipo == 'Mesa Redonda' ? 'Moderador: ' : 'Ministrante: '}
+                          </Text>
+                          <Text
+                            fontSize='2x1'
+                            display={dados.tipo == 'VGO' ? 'none' : ''}
+                          >
+                            {dados.ministrante}
+                            <br /><br />
+                          </Text>
+                          <Text
+                            fontSize='2x1'
+                            color='#cc6600'
+                          >
+                            Descrição:
+                        </Text>
+                          <Text fontSize='2x1'>
+                            {dados.desc}
+                            <br /><br />
+                          </Text>
+                          <Text
+                            fontSize='2x1'
+                            color='#cc6600'
+                          >
+                            Plataforma:
+                        </Text>
+                          <Text fontSize='2x1'>
+                            {dados.plataforma}
+                          </Text>
+                        </AccordionPanel>
+                      </AccordionItem>
+                    ))}
 
-                </Accordion>
-              </TabPanel>
-            ))
-          }
-        </TabPanels>
-      </Tabs>
-    </Heading>
+                  </Accordion>
+                </TabPanel>
+              ))
+            }
+          </TabPanels>
+        </Tabs>
+      </Box >
+    </Box>
   )
 }
 
