8a7c34e119a895975fc3a8899db3d81f3bcdc499
Felipe Keiler <fkeiler@PythonBlack.localdomain>
2020-08-19 21:51:39 -0300

    ✨ Seção "sobre" populada

--- src/components/sobre.jsx
+++ src/components/sobre.jsx
@@ -1,10 +1,81 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/core';
+import {
+  Flex,
+  Heading,
+  PseudoBox,
+  Text }
+  from '@chakra-ui/core';
+import { keyframes } from '@emotion/core';
+
+const glitch = keyframes`
+  0% {
+    transform: translateX(0%) translateY(0%);
+  }
+  20% {
+    transform: translateX(-2%) translateY(-2%);
+  }
+  40% {
+    transform: translateX(-2%) translateY(2%);
+  }
+  60% {
+    transform: translateX(2%) translateY(-2%);
+  }
+  80% {
+    transform: translateX(2%) translateY(2%);
+  }
+  100% {
+    transform: translateX(0%) translateY(0%);
+  }
+`;
 
 const Sobre = () => (
-  <Heading size="lg">
-    Sobre
-  </Heading>
+  <Flex direction="column" alignItems="center" backgroundColor="cyan.500">
+    <Heading size="xl" my={5}>
+      Semana Acadêmica da Computação
+    </Heading>
+    <Text maxW="90ch" my={3}>
+      A SAC é um evento anual organizado pelo PET Computação como projeto de extensão do
+      Departamento de Computação da UFC. A SAC explora as diversas áreas da computação, como Data
+      Science, Empreendedorismo e Desenvolvimento Web através de palestras, workshops e mesas
+      redondas, onde alunos de Computação (e outras pessoas interessadas) podem aprender ou
+      apresentar tópicos que lhe interessam.
+    </Text>
+    <Heading size="xl" my={5}>
+      SAC
+      <PseudoBox 
+        display="inline"
+        position="relative"
+        zIndex="2"
+        _before={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          content: '" Virtual?"',
+          color: 'rgba(255, 0, 255, 0.6)',
+          animation: `${glitch} 1s infinite`,
+          zIndex: -2,
+        }}
+        _after={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          content: '" Virtual?"',
+          color: 'rgba(0, 255, 255, 0.6)',
+          animation: `${glitch} 1s .4s infinite`,
+          zIndex: -1,
+        }}
+      >
+        &nbsp;Virtual?
+      </PseudoBox>
+    </Heading>
+    <Text maxW="90ch" mt={2} mb={5}>
+      Em decorrência da pandemia do coronavírus, a XV SAC será virtual, para você poder
+      acompanhar no conforto da sua casa, sem o risco da pandemia. A SAC ainda contará com diversas
+      palestras dos mais variados tópicos, com convidados de todo o Brasil.
+      <br />
+      (E não se preocupem, a VGO não foi cancelada 😉)
+    </Text>
+  </Flex>
 );
 
 export default Sobre;
