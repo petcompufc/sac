dd19738432b01c07d3faeeb3af11ec88118cae39
rafaelFacundo <79310598+rafaelFacundo@users.noreply.github.com>
2021-10-19 21:26:27 -0300

    Add files via upload
    Atualizando dados das atividades para a XVI SAC (2021)

--- src/data/atividades.js
+++ src/data/atividades.js
@@ -1,123 +1,98 @@
 const cronograma = [
   {
-    dia: 'Terça-feira (08/09)',
+    dia: 'Segunda-feira (25/10)',
     atividades: [
       {
         tipo: 'Palestra',
-        titulo: 'Data Science',
-        ministrante: 'Artur Barbosa',
-        desc: '',
+        titulo: 'DuckDB: Um SGBD embarcado para análise/ciência de dados',
+        ministrante: 'Pedro Holanda',
+        desc: 'Nesta palestra eu apresentarei uma visão geral das tecnologias utilizadas no DuckDB (https://duckdb.org/). Explicarei o que o torna único como uma ferramenta essencial no ecossistema de bibliotecas para ciência de dados. Por fim, apresentarei um exemplo prático de uso do DuckDB, e compararei com outras ferramentas de  manuseio de dados (e.g., Pandas). O DuckDB possui código aberto (https://github.com/duckdb/duckdb) e já está disponível para download e uso.',
         plataforma: 'Google Meet e YouTube',
         hora: '16:00 - 16:50',
       },
       {
         tipo: 'Palestra',
-        titulo: 'Dados de Trajetória: O que são? O que comem? O que fazer com eles?',
-        ministrante: 'Andreza Fernandes',
-        desc: `Você já reparou que ultimamente nós tivemos um grande avanço e barateamento dos custos de tecnologias de localização?! Isso acabou acarretando um grande aumento no volume de dados de trajetória! E esse tipo de dado é bastante importante, viu?!
-
-        Imagina o tanto que ele pode auxiliar no entendimento do tráfego de pessoas, de locais que elas mais visitam, encontrar padrões em seus percursos... Como sempre, estamos aqui para facilitar a vida das pessoas, dessa vez as de profissionais de Urbanismo, Geografia e diversas outras áreas!
-
-        Com toda essa importância, nessa palestra iremos: 
-        - Entender melhor esse tipo de dado;
-        - O que podemos fazer e como podemos brincar com eles; 
-        - Apresentar uma biblioteca Python open-source que contém todo o pipeline para trabalho com esse tipo de dado: o PyMove! 
-        
-        Ficou curioso?! Não perde o nosso encontro!`,
+        titulo: 'Introdução ao WebRTC: comunicação em tempo real sem complicações',
+        ministrante: 'Diego Cândido',
+        desc: 'O WebRTC é uma tecnologia web que permite que dois ou mais dispositivos transmitam áudio e vídeo sem a necessidade de um servidor intermediário.',
+        plataforma: 'Google Meet e YouTube',
+        hora: '17:00 ',
+      },
+    ],
+  },
+  {
+    dia: 'Terça-feira (26/10)',
+    atividades: [
+      {
+        tipo: 'Palestra',
+        titulo: 'Mercado de Trabalho de Computação.',
+        ministrante: 'Ariana Lopes e Heitor Oliveira',
+        desc: 'Uma palestra com dois ex alunos da UFC, com experiência no mercado de trabalho, que hoje estão trabalhando em grandes empresas, vão contar sobre suas experiências, dar dicas sobre carreira e falar sobre o mercado de trabalho',
+        plataforma: 'Google Meet e YouTube',
+        hora: '16:00 - 17:20',
+      },
+      {
+        tipo: 'Workshop',
+        titulo: 'react-query',
+        ministrante: 'Hugo Duarte',
+        desc: 'Um Workshop com bastante prática para aqueles que querem iniciar nestas ferramentas ou aprender mais sobre desenvolvimento web.',
+        plataforma: 'Google Meet e YouTube',
+        hora: '17:30 - 18:30',
+      },
+    ],
+  },
+  {
+    dia: 'Quarta-feira (27/10)',
+    atividades: [
+      {
+        tipo: 'Palestra',
+        titulo: 'Projeto ELIOT',
+        ministrante: 'Carlos Eduardo Pinheiro',
+        desc: 'O projeto eliot, consiste num conjunto de ferramentas(aplicativo e site web) para suporte à Avaliação Longitudinal de Aplicações de Internet das Coisas. A apresentação abordará sobre detalhes do projeto e de onde surgiu a ideia,além  mostrar aos espectadores como funciona uma bolsa de pesquisa e tecnologia na universidade. Haverá informações sobre IOT e desenvolvimento de sistemas. O objetivo principal é apresentar o projeto e fazer com que os graduandos se interessem por pesquisa e desenvolvimento.',
+        plataforma: 'Google Meet e YouTube',
+        hora: '16:00 - 16:50',
+      },
+      {
+        tipo: 'Palestra',
+        titulo: 'Biossinais, ECG e reconhecimento de padrões, uma importante aplicação de técnicas de aprendizagem de máquina.',
+        ministrante: 'João Paulo',
+        desc:'A doença de Chagas (DC), também conhecida como tripanossomíase americana, ocorre em mais de 21 países, principalmente na América Latina (no Brasil e no México). O número de infectados gira de 6 a 7 milhões de pessoas . A disseminação da doença geralmente ocorre quando as fezes dos insetos da subfamília Triatominae, portadores do protozoário Trypanosoma cruzi, entram em contato com o sangue do paciente. A infecção, então, ocorre em duas fases: aguda e crônica. De acordo com a Organização Mundial de Saúde (OMS), durante a fase crônica, 30% dos pacientes desenvolvem algum tipo de cardiomiopatia, como arritmias cardíacas ou insuficiência cardíaca progressiva, que, se não tratadas, podem levar o paciente chagásico à Morte Súbita Cardíaca (MSC). A MSC, por ser um dos principais resultados adversos causados pela doença de Chagas, tem chamado a atenção dos pesquisadores nos últimos anos. Diversos estudos foram propostos para a identificação de variáveis clínicas e laboratoriais que auxiliem na detecção precoce de uma possível alta propensão à MSC. Neste contexto, o presente projeto de pesquisa propõe a aplicação de técnicas de processamento digital de biossinais, bem como a utilização de algoritmos do estado da arte de aprendizagem de máquina, técnicas de subamostragem e sobreamostragem e análise exploratória de dados, sobre uma base de dados clínicos e de sinais ECG de pacientes chagásicos, objetivando a classificação em alta ou baixa predisposição do paciente à MSC. Seleção de atributos e balanceamento dos dados são aplicados para lidar com o problema do desbalanceamento. Estes algoritmos são otimizados sobre uma amostragem de 310 pacientes, dos quais 78 são positivos para MSC.',
         plataforma: 'Google Meet e YouTube',
         hora: '17:00 - 17:50',
       },
     ],
   },
   {
-    dia: 'Quarta-feira (09/09)',
+    dia: 'Quinta-feira (28/10)',
     atividades: [
       {
         tipo: 'Palestra',
-        titulo: 'A Insegurança na Web',
-        ministrante: 'Ramon Martins',
-        desc: '',
+        titulo: 'Qual modelo escolher? Conheça o H2O AutoML',
+        ministrante: 'João Castelo Branco',
+        desc: 'Durante o desenvolvimento de uma aplicação baseada em inteligência artificial, nos deparamos sempre com a seguinte pergunta: Qual modelo escolher? Para respondê-la um pouco mais rápido, existem aplicações que implementam Automatic Machine Learning, um grande teste com diversos modelos e parâmetros é feito sobre os dados para escolher o melhor deles para seu problema. Conheceremos a ferramenta mais completa do mercado, o H2O AutoML e veremos em primeira mão as vantagens do seu uso.',
         plataforma: 'Google Meet e YouTube',
         hora: '16:00 - 16:50',
       },
       {
         tipo: 'Palestra',
-        titulo: 'Fazer um App aplicando API de Geolocalização em React!',
-        ministrante: 'CEOS',
-        desc: '',
+        titulo: 'Introdução a Computação Forense e a Perícia Digital',
+        ministrante: 'Marcus Fábio Fontenelle',
+        desc: 'Segundo estudo da consultoria alemã Roland Berger, o Brasil sofreu mais de 9 milhões de ataques no primeiro semestre de 2021, mais do que todo o ano de 2020, ficando atrás apenas dos EUA, Reino Unido, Alemanha e África do Sul. Se formos analisar apenas ataques de Ransomware, de acordo com relatório da empresa Bitdefender, o Brasil encontra-se em 3º lugar. Tais estatísticas trazem uma grande oportunidade para profissionais da área de tecnologia e com conhecimentos necessários para realizar uma investigação digital ajudando a encontrar a causa raiz e a autoria de ataques cibernéticos. Na palestra serão abordados os principais conceitos da computação forense, o escopo de atuação do perito e os desafios inerentes à atividade.',
         plataforma: 'Google Meet e YouTube',
         hora: '17:00 - 17:50',
       },
     ],
   },
   {
-    dia: 'Quinta-feira (10/09)',
-    atividades: [
-      {
-        tipo: 'Palestra',
-        titulo: 'Introdução a CTF',
-        ministrante: 'Igor Melo',
-        desc: '',
-        plataforma: 'Google Meet e YouTube',
-        hora: '16:00 - 16:50',
-      },
-      {
-        tipo: 'Palestra',
-        titulo: 'Reprogramando Soft Skills para o Mercado de Tecnologia',
-        ministrante: 'Lucas Fernando',
-        desc: '',
-        plataforma: 'Google Meet e YouTube',
-        hora: '17:00 - 17:50',
-      },
-    ],
-  },
-  {
-    dia: 'Sexta-feira (11/09)',
-    atividades: [
-      {
-        tipo: 'Palestra',
-        titulo: 'A Industria de Desenvolvimento de Jogos: Minha Experiência',
-        ministrante: 'Celito Felipetto',
-        desc: '',
-        plataforma: 'Google Meet e YouTube',
-        hora: '16:00 - 16:50',
-      },
-      {
-        tipo: 'Palestra',
-        titulo: 'Mercado de Software: História de Carreira',
-        ministrante: 'Pietro Valente',
-        desc: '',
-        plataforma: 'Google Meet e YouTube',
-        hora: '17:00 - 17:50',
-      },
-      {
-        tipo: 'VGO',
-        titulo: 'Gartic + Webgames',
-        ministrante: '',
-        desc: '',
-        plataforma: '',
-        hora: '17:50 - 00:00',
-      },
-    ],
-  },
-  {
-    dia: 'Sábado (12/09)',
+    dia: 'Sábado (30/10)',
     atividades: [
       {
         tipo: 'VGO',
-        titulo: 'Campeonato de LOL',
+        titulo: 'Campeonato de CS:GO',
         ministrante: '',
-        desc: 'Monte sua equipe de até 5 jogadores e venha participar! Veja o nosso regulamento: https://docs.google.com/document/d/1phR-G2JfRXV_75gnJw46PKy_saEpRO02l_N4hp72iZ0',
+        desc: 'Monte sua equipe de até 5 jogadores e venha participar! Veja o nosso regulamento: Falta só colocar o link ',
         plataforma: 'Discord',
-        hora: '12:00 - 17:00',
-      },
-      {
-        tipo: 'VGO',
-        titulo: 'Campeonato de Valorant',
-        ministrante: '',
-        desc: 'Monte sua equipe de até 5 jogadores e venha participar! Veja o nosso regulamento: https://docs.google.com/document/d/1O3t-uKYNfoH3M5Fw9r-eUljW-7wEUfVXe7cul2OKS9s',
-        plataforma: 'Discord',
-        hora: '17:30 - 22:00',
+        hora: '16:00 - 17:50',
       },
     ],
   },
@@ -126,11 +101,11 @@ const cronograma = [
     atividades: [
       {
         tipo: 'VGO',
-        titulo: 'Campeonato de CS:GO',
+        titulo: 'Campeonato de Stopots',
         ministrante: '',
-        desc: 'Monte sua equipe de até 5 jogadores e venha participar! Veja o nosso regulamento: https://docs.google.com/document/d/1AwD7e60J7JZ0H_pehpTRxEo2xmdNRIGqDBIK8-J6Xg8',
+        desc: 'Monte sua equipe de até 5 jogadores e venha participar! Veja o nosso regulamento: Falta só colocar o link ',
         plataforma: 'Discord',
-        hora: '12:00 - 17:00',
+        hora: '16:00 - 17:50',
       },
     ],
   },
